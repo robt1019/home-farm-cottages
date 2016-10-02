9509ff4e4f754de9e3cba2df61966fd189c64559
robt1019 <robert.i.taylor88@gmail.com>
2016-10-02 16:40:38 +0100

    moved folders around

--- karma.conf.js
+++ karma.conf.js
@@ -18,10 +18,10 @@ module.exports = function(config) {
             'bower_components/angular/angular.js',
             'bower_components/angular-route/angular-route.js',
             'bower_components/angular-mocks/angular-mocks.js',
-            'app.js',
-            'homepage/**/*.js',
-            'navbar/**/*.js',
-            '**/*.html',
+            'public/app.js',
+            'public/homepage/**/*.js',
+            'public/navbar/**/*.js',
+            'public/**/*.html',
             'test/unit/**/*.js'
         ],
 
