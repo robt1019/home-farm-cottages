b9893b2d7feae53b0a8323423856d36de6237807
robt1019 <robert.i.taylor88@gmail.com>
2016-10-02 12:42:35 +0100

    added placeholder favicon and added dummy /about and /contact routes to frontend. removed jade templates

--- public/routes.js
+++ public/routes.js
@@ -1,5 +1,13 @@
-angular.module('hfc').config(function($routeProvider) {
+angular.module('hfc').config(function($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
         templateUrl: 'homepage/homepage.view.html'
+    })
+    .when('/about', {
+        template: '<h1>About</h1>'
+    })
+    .when('/contact', {
+        template: '<h1>Contact</h1>'
     });
+
+    $locationProvider.html5Mode(true);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/*GET home page */
-router.get('/', function(req, res, next) {
-    res.render('homepage', {title: 'Home Farm Cottages'});
-});
+// /*GET home page */
+// router.get('/', function(req, res, next) {
+//     res.render('homepage', {title: 'Home Farm Cottages'});
+// });
 
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
-});
+// /* GET about page. */
+// router.get('/about', function(req, res, next) {
+//   res.render('about', { title: 'About' });
+// });
     
 module.exports = router;
